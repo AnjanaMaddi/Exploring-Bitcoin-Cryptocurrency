78e31a6190e9792910f46e9ded795fc62b6ba7dd
AnjanaMaddi <55125756+AnjanaMaddi@users.noreply.github.com>
2020-08-04 07:43:40 +0530

    Update code.py

--- code.py
+++ code.py
@@ -31,6 +31,94 @@ plt.ylabel(TOP_CAP_YLABEL)
 plt.savefig('Top-10-market-capitalization.png', dpi=300, bbox_inches='tight')
 plt.show()
 
+volatility = dec6[['id', 'percent_change_24h','percent_change_7d']]
+v_24h = volatility.query('percent_change_24h != \'NaN\'')
 
-#volatile natute of Bitcoins
-#change in 24 hours
+TOP_CAP_TITLE = 'Top 10 highly volatile cryptocurrencies in a day'
+TOP_CAP_YLABEL = '% change in 24 hrs'
+v_24h_top_10=v_24.head(10)
+v_24h_top_10.plot.bar(x='id',y='percent_change_24h') 
+ax.xaxis.set_tick_params(pad = 5) 
+ax.yaxis.set_tick_params(pad = 2)
+plt.ylabel(TOP_CAP_YLABEL)
+plt.title(TOP_CAP_TITLE)
+# Plotting the barplot with the title defined above 
+plt.savefig('Top-10-highly-volatile-cryptocurrencies-in-a-day.png', dpi=300, bbox_inches='tight')
+plt.show()
+
+TOP_CAP_TITLE = '24 hours top losers'
+TOP_CAP_YLABEL = '% change in 24 hrs'
+v_24h_top_10=v_24.tail(10)
+v_24h_top_10.plot.bar(x='id',y='percent_change_24h') 
+ax.xaxis.set_tick_params(pad = 5) 
+ax.yaxis.set_tick_params(pad = 2)
+plt.ylabel(TOP_CAP_YLABEL)
+plt.title(TOP_CAP_TITLE)
+# Plotting the barplot with the title defined above 
+plt.savefig('24-hours-top-losers.png', dpi=300, bbox_inches='tight')
+plt.show()
+
+v_w=volatility.query('percent_change_7d != \'NaN\'')
+v_w=v_w.sort_values(by='percent_change_7d',ascending=False)
+
+TOP_CAP_TITLE = '1 week top winners'
+TOP_CAP_YLABEL = '% change in a week'
+v_w_top_10=v_w.head(10)
+v_w_top_10.plot.bar(x='id',y='percent_change_7d') 
+ax.xaxis.set_tick_params(pad = 5) 
+ax.yaxis.set_tick_params(pad = 2)
+plt.ylabel(TOP_CAP_YLABEL)
+plt.title(TOP_CAP_TITLE)
+# Plotting the barplot with the title defined above 
+plt.savefig('1-week-top-winners.png', dpi=300, bbox_inches='tight')
+plt.show()
+
+TOP_CAP_TITLE = '1 week top losers'
+TOP_CAP_YLABEL = '% change in a week'
+v_w_top_10=v_w.tail(10)
+v_w_top_10.plot.bar(x='id',y='percent_change_7d') 
+ax.xaxis.set_tick_params(pad = 5) 
+ax.yaxis.set_tick_params(pad = 2)
+plt.ylabel(TOP_CAP_YLABEL)
+plt.title(TOP_CAP_TITLE)
+# Plotting the barplot with the title defined above 
+plt.savefig('1-week-top-losers.png', dpi=300, bbox_inches='tight')
+plt.show()
+
+top_cap=dec6.sort_values(by='market_cap_usd',ascending=False)
+
+TOP_CAP_TITLE = '24 hour change for top cryptocurencies'
+TOP_CAP_YLABEL = '% change in a day'
+v_w_top_10=top_cap.head(10)
+v_w_top_10.plot.bar(x='id',y='percent_change_24h') 
+ax.xaxis.set_tick_params(pad = 5) 
+ax.yaxis.set_tick_params(pad = 2)
+plt.ylabel(TOP_CAP_YLABEL)
+plt.title(TOP_CAP_TITLE)
+# Plotting the barplot with the title defined above 
+plt.savefig('24-hour-change-for-top-cryptocurencies.png', dpi=300, bbox_inches='tight')
+plt.show()
+
+TOP_CAP_TITLE = '24 hour change for top cryptocurencies'
+TOP_CAP_YLABEL = '% change in a day'
+v_w_top_10=top_cap.head(10)
+v_w_top_10.plot.bar(x='id',y='percent_change_24h') 
+ax.xaxis.set_tick_params(pad = 5) 
+ax.yaxis.set_tick_params(pad = 2)
+plt.ylabel(TOP_CAP_YLABEL)
+plt.title(TOP_CAP_TITLE)
+# Plotting the barplot with the title defined above 
+plt.savefig('24-hour-change-for-top-cryptocurencies.png', dpi=300, bbox_inches='tight')
+plt.show()
+
+TOP_CAP_TITLE = '1 week change for top cryptocurencies'
+TOP_CAP_YLABEL = '% change in a day'
+v_w_top_10=top_cap.head(10)
+v_w_top_10.plot.bar(x='id',y='percent_change_7d') 
+ax.xaxis.set_tick_params(pad = 5) 
+ax.yaxis.set_tick_params(pad = 2)
+plt.ylabel(TOP_CAP_YLABEL)
+plt.title(TOP_CAP_TITLE)
+# Plotting the barplot with the title defined above 
+plt.savefig('1-week-change-for-top-cryptocurencies.png', dpi=300, bbox_inches='tight')
+plt.show()
